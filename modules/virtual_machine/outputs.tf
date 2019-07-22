9d3d5a10a22d120b1d517ebf8b297fd95dce1a15
Edmilson Alferes <edmilson.alferes@kantaribopemedia.com>
2019-07-22 11:34:47 -0300

    add output admin_username

--- modules/virtual_machine/outputs.tf
+++ modules/virtual_machine/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   value = "${azurerm_virtual_machine.virtual_machine.id}"
   description = "The ID of the Virtual Machine."
 }
+
+output "admin_username" {
+  value = "${azurerm_virtual_machine.virtual_machine.admin_username}"
+  description = "The name of the administrator account."
+}
