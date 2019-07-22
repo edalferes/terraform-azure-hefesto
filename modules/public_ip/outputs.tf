ed60733a80def4d107c0de29e136cf95cdda3af1
Edmilson Alferes <edmilson.alferes@kantaribopemedia.com>
2019-07-22 11:24:52 -0300

    add output fqdn

--- modules/public_ip/outputs.tf
+++ modules/public_ip/outputs.tf
@@ -8,3 +8,7 @@ output "ip_address" {
   description = "The IP address value that was allocated."
 }
 
+output "fqdn" {
+  value       = "${azurerm_public_ip.public_ip.fqdn}"
+  description = "Fully qualified domain name of the A DNS record associated with the public IP. domain_name_label must be specified to get the fqdn. This is the concatenation of the domain_name_label and the regionalized DNS zone"
+}
