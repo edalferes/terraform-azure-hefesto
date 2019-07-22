6728a0eb8f137d128af371abf58c86a5d69d1557
Edmilson Alferes <edmilson.alferes@kantaribopemedia.com>
2019-07-22 11:41:15 -0300

    remove outputs

--- modules/virtual_machine/outputs.tf
+++ modules/virtual_machine/outputs.tf
@@ -2,8 +2,3 @@ output "id" {
   value = "${azurerm_virtual_machine.virtual_machine.id}"
   description = "The ID of the Virtual Machine."
 }
-
-output "admin_username" {
-  value = "${azurerm_virtual_machine.virtual_machine.admin_username}"
-  description = "The name of the administrator account."
-}
