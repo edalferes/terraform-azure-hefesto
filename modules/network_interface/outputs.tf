f4323a32bc378da1f9cf73cdc0ee194189a60fee
Edmilson Alferes <edmilson.alferes@kantaribopemedia.com>
2019-07-26 15:52:44 -0300

    add new values variables

--- modules/network_interface/outputs.tf
+++ modules/network_interface/outputs.tf
@@ -1,3 +1,9 @@
 output "id" {
   value = "${azurerm_network_interface.network_interface.id}"
+  description = "The Virtual Network Interface ID."
+}
+
+output "private_ip_address" {
+  value = "${azurerm_network_interface.network_interface.private_ip_address}"
+  description = "The first private IP address of the network interface."
 }
